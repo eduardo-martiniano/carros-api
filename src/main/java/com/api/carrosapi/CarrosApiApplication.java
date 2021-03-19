24d1c3683c2fc47be3b3be067faf0a4588b03a57
Eduardo Martiniano <eeduardo.martiniano@gmail.com>
2021-03-19 17:53:05 -0300

    Project created

---
+++ src/main/java/com/api/carrosapi/CarrosApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.carrosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosApiApplication.class, args);
+	}
+
+}
