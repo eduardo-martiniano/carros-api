ab21f85c0af34f316f2e365a84db59dcaeee84c2
Eduardo Martiniano <eeduardo.martiniano@gmail.com>
2021-03-20 21:39:19 -0300

    crud car

--- src/main/java/com/api/carrosapi/Controllers/CarController.java
+++ src/main/java/com/api/carrosapi/Controllers/CarController.java
@@ -1,10 +1,18 @@
 package com.api.carrosapi.Controllers;
 
+import java.util.Optional;
+
 import com.api.carrosapi.Domain.Entities.Car;
 import com.api.carrosapi.Domain.Services.ICarService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +25,48 @@ public class CarController {
     private ICarService _carService;
 
     @PostMapping()
-    public Car addCar(@RequestBody Car car) {
-        return _carService.save(car);
+    public ResponseEntity<?> addCar(@RequestBody Car car) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(_carService.save(car));
+    }
+    
+    @GetMapping()
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(_carService.getAll());
+    }
+    
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") Long id) {
+        Optional<Car> car = _carService.getById(id);
+        if (car.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi possivel encontrar esse carro");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(car);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") Long id, @RequestBody Car car) {
+        Optional<Car> _car = _carService.getById(id);
+        if (_car.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi possivel encontrar esse carro");
+        }
+        
+        Boolean result = _carService.update(id, car);
+        
+        if (!result) {
+            return ResponseEntity.status(500).body("Erro ao editar carro :(");
+        }
+        
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Carro editado com sucesso!");
+    }
+    
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> remove(@PathVariable("id") Long id) {
+        Optional<Car> _car = _carService.getById(id);
+        
+        if (_car.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi possivel encontrar esse carro");
+        }
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Carro removido com sucesso!");
     }
 }
--- src/main/java/com/api/carrosapi/Domain/Services/ICarService.java
+++ src/main/java/com/api/carrosapi/Domain/Services/ICarService.java
@@ -1,11 +1,12 @@
 package com.api.carrosapi.Domain.Services;
 
+import java.util.Optional;
+
 import com.api.carrosapi.Data.ICarRepository;
 import com.api.carrosapi.Domain.Entities.Car;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import lombok.Data;
 
 @Data
@@ -17,5 +18,30 @@ public class ICarService {
     
     public Car save(Car car) {
         return _carRepository.save(car);
+    }
+
+    public Iterable<Car> getAll() {
+        return _carRepository.findAll();
+    }
+
+    public Optional<Car> getById(Long id) {
+        return _carRepository.findById(id);
+    }
+
+    public boolean update(Long id, Car car) {
+        return _carRepository.findById(id)
+           .map(record -> {
+               record.setName(car.getName());
+               record.setColor(car.getColor());
+               record.setPrice(car.getPrice());
+               record.setQuantityInStock(car.getQuantityInStock());
+               _carRepository.save(record);
+               return true;
+           }).orElse(false);
+    }
+
+    public void remove(Long id) {
+        Car car = _carRepository.findById(id).get();
+        _carRepository.delete(car);
     }
 }
